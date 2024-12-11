1d3c2bfb6d56750b94b62232cb87f2224d5dd5d4
RubénCano21 <ruben217.rc@gmail.com>
2024-12-11 16:58:09 -0400

    chatbot

--- src/main/java/uagrm/bo/workflow/WorkflowApplication.java
+++ src/main/java/uagrm/bo/workflow/WorkflowApplication.java
@@ -1,13 +1,22 @@
 package uagrm.bo.workflow;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkflowApplication {
 
+    private static final Dotenv dotenv = Dotenv.load();
+
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
+
+
+    }
+
+    public static String getEnv(String key) {
+        return dotenv.get(key);
     }
 
 }
